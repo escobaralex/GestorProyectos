4d849f3f50094b2c41165a37a470b1e6e96fb460
escobaralex <aescobar@tadis.cl>
2016-10-04 01:30:15 -0300

    Se agrega comprobación de permisos por cada llamada a la api consumiendo procedimiento almacenada hasPermission

--- api/policies/hasPermission.js
+++ api/policies/hasPermission.js
@@ -9,7 +9,7 @@
 module.exports = function (req, res, next) {  
   var verb = req.method;
   var method = req.options.action;
-  
+  var hasPermission = false;
   var _userId = req.token.id;
   var _module = req.options.controller;    
   var _action = null;
@@ -30,13 +30,25 @@ module.exports = function (req, res, next) {
   if(!_action || null == _action)
     return res.json(401, {err: 'Error al intentar verificar los permisos del usuario'});
     
-  User.query({
-    text: "CALL HAS_PERMISSION($1,$2)",
+  /*User.query({
+    text: "CALL `HasPermission`($1,$2);",
     values: [ _userId, _module ]
-  }, function(err, result) {
-    if(!result || null == result || result.length == 0 || result[0][_action])
+
+  },*/
+  User.query("CALL `HasPermission`(" + _userId + "," + "'"+_module+"');",[], function(err, result) {
+    if(!result || null == result || result.length == 0){
+      sails.log.error(err);
       return res.json(401, {err: 'No cuenta con permisos para realizar esta acción.'});
-    
-    next();
+    }else{  
+      for(var i = 0; i < result.length; i++){
+        if(result[0][i][_action])
+          hasPermission = true;    
+      }
+      if(hasPermission){
+        next();
+      }else{
+        return res.json(401, {err: 'No cuenta con permisos para realizar esta acción.'});
+      }
+    }    
   });
 };
--- config/bootstrap.js
+++ config/bootstrap.js
@@ -331,7 +331,7 @@ module.exports.bootstrap = function(cb) {
     //init_recurso,    
     init_usuarios,
     init_role_profile,
-    setStoreProcedures
+    //setStoreProcedures
   ], cb);
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
